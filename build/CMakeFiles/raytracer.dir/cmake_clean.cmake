e60e5fcccf5b95d995b0d861fe31fcf07ea3e2c1
irregularBismuth <elliot.s@live.se>
2024-01-27 23:19:53 +0100

    startup files

---
+++ build/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/main.cpp.o"
+  "CMakeFiles/raytracer.dir/main.cpp.o.d"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
