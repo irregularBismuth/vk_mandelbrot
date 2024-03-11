54ef6317a196ce731ada088dfaeb7060833263d4
irregularBismuth <elliot.s@live.se>
2024-03-11 22:56:37 +0100

    added instance start

--- source/include/graphics.h
+++ source/include/graphics.h
@@ -2,18 +2,21 @@
 #define GRAPHICS_H
 #include <vulkan/vulkan.h>
 #include <precomp.h>
+#include <glfw_window.h>
 
 namespace veng {
   class Graphics final {
     public:    
       Graphics(gsl::not_null<Window*> window);
-      ~Graphics() = default;
+      ~Graphics();
      
-      void InitilizeVulkan();
-      void CreateInstance();
     private:
+
+      gsl::span<gsl::czstring> GetSuggestedExtensions();   
+      void CreateInstance();
+      void InitilizeVulkan();
       VkInstance instance_;
       gsl::not_null<Window*> window_;
   };
 }
-endif 
+#endif 
--- source/src/graphics.cpp
+++ source/src/graphics.cpp
@@ -1,7 +1,53 @@
 #include "graphics.h"
 
+namespace veng {
 
 Graphics::Graphics(gsl::not_null<Window*> window) : window_(window) {
-   VkApplicationInfo app_info{}; 
+  InitilizeVulkan();
 }
 
+Graphics::~Graphics() {
+  if(instance_ != nullptr) {
+    vkDestroyInstance(instance_,nullptr);
+  }
+}
+
+
+void Graphics::InitilizeVulkan() {
+  CreateInstance();
+}
+
+void Graphics::CreateInstance() {
+  gsl::span<gsl::czstring> suggested_extensions = GetSuggestedExtensions();
+
+  VkApplicationInfo app_info = {};
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pNext = nullptr;
+  app_info.pApplicationName = "VK_MANDELBROT";
+  app_info.applicationVersion = VK_MAKE_VERSION(1,0,0);
+  app_info.pEngine = "veng";
+  app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  app_info.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo instance_creation_info = {};
+  instance_creation_info.sType = VK_STRUCTURE_TYPE_CREATE_INFO;
+  instance_creation_info.pNext = nullptr;
+  instance_creation_info.pApplicationInfo = &app_info;
+  instance_creation_info.enabledExtensionCount = suggested_extensions.size();
+  instance_creation_info.ppEnabledExtensionNames = suggested_extensions.data();
+  instance_creation_info.enabledLayerCount = 0;
+
+  VkResult result = vkCreateInstance(&instance_creation_info,nullptr,&instance);
+  if(result != VK_SUCCESS) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+gsl::span<gsl::czstring> Graphics::GetSuggestedExtensions() {
+  std::uint32_t glfwExtensionCount = 0;
+  gsl::czstring *glfwExtensions;
+  glfwExtensions = gflwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  return {glfw_extensions,glfw_extension_count};
+
+}   
+}
